d5fef1956eaa43c01d117a54ec5251d44dab4a33
VictorCottar <victorcottarms@outlook.com>
2025-02-19 21:45:47 -0300

    Refatoração da controller de autentificação

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,61 +1,36 @@
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcryptjs';
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
-const SECRET_KEY = process.env.SECRET_KEY;
+import { registerUser, loginUser, verifyToken } from '../services/authService.js';
 
 export const register = async (req, res) => {
-    const { email, password } = req.body;
-
-    const existingUser = await prisma.user.findUnique({ where: { email } });
-    if (existingUser) {
-        return res.status(400).json({ message: 'Usuário já cadastrado' });
+    try {
+        const { email, password } = req.body;
+        const token = await registerUser(email, password);
+        res.json({ token });
+    } catch (error) {
+        res.status(400).json({ message: error.message });
     }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const user = await prisma.user.create({
-        data: {
-            email,
-            password: hashedPassword
-        },
-    });
-
-    const token = jwt.sign({ id: user.id }, SECRET_KEY, { expiresIn: '1h' });
-    res.json({ token });
-}
+};
 
 export const login = async (req, res) => {
-    const { email, password } = req.body;
-
-    const user = await prisma.user.findUnique({ where: { email } });
-    if (!user) {
-        return res.status(400).json({ message: 'Credenciais inválidas' });
+    try {
+        const { email, password } = req.body;
+        const token = await loginUser(email, password);
+        res.json({ token });
+    } catch (error) {
+        res.status(400).json({ message: error.message });
     }
-    console.log('user', user);
+};
 
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-    if (!isPasswordValid) {
-        return res.status(400).json({ message: 'Senha incorreta' });
-    }
-    console.log('isPasswordValid', isPasswordValid);
-
-    const token = jwt.sign({ id: user.id }, SECRET_KEY, { expiresIn: '1h' });
-    res.json({ token });
-}
-
-export const authenticateToken = (req, res, next) => {
+export const authenticateToken = async (req, res, next) => {
     const token = req.headers['authorization'];
     if (!token) {
         return res.status(401).json({ error: 'Token não fornecido' });
     }
 
-    jwt.verify(token, SECRET_KEY, (err, user) => {
-        if (err) {
-            return res.status(403).json({ error: 'Token inválido' });
-        }
+    try {
+        const user = await verifyToken(token);
         req.user = user;
         next();
-    });
+    } catch (error) {
+        res.status(403).json({ error });
+    }
 };
